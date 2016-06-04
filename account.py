e1826ac8a95b5bf4b675f54774801456dee74d2b
bendmahon <bendmahon@gmail.com>
2016-06-04 12:50:24 -0500

    structure update

--- account.py
+++ account.py
@@ -1,12 +1,11 @@
 class Account:
-	def __init__(self, funds, interest_rate):
-		self.funds = funds
-		self.interest_rate = interest_rate
+    def __init__(self, funds, interest_rate):
+        self.funds = funds
+        self.interest_rate = interest_rate
 
-
-	@property
-	def has_funds(self):
-		return self.funds > 0
+    @property
+    def has_funds(self):
+        return self.funds > 0
 
 my_account = Account(300, 0.1)
-print my_account.has_funds
+print(my_account.has_funds)
--- universal.py
+++ universal.py
@@ -2,6 +2,6 @@ from account import Account
 
 
 class UniversalBank:
-	def __init__(self):
-		self.account = Account(1000, 0)
-		self.scenarios = []
+    def __init__(self):
+        self.account = Account(1000, 0)
+        self.scenarios = []
